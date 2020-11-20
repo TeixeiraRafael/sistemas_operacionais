fc54a8c26ffd467a802a7de57b7dfd8be18653b1
Rafael Teixeira <rodriguesteixeirarafael@gmail.com>
2020-11-20 14:55:37 -0300

    Uploading old ex1.c implementation

---
+++ threads/c/trabalho1/ex1_old.c
@@ -0,0 +1,160 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <time.h>
+#include <stdlib.h>
+
+/*
+	The segment struct holds a portion of an array.
+	Each thread inverts a single array segment.
+*/
+typedef struct segment{
+	int thread_id;
+	int size;
+	int* values;
+} Segment;
+
+
+void print_array(int* arr, int size){
+	printf("[");
+	for(int i = 0; i < size; i++){
+		if(i < size-1)
+			printf("%d, ", arr[i]);
+		else
+			printf("%d]\n", arr[i]);
+	}	
+}
+
+int* random_array(int size){
+	/*
+		Declaring the entry pointer of the array as a static variable so it won't
+		go out of scope when the stack frame of the function gets popped.
+	*/	
+	static int* array;
+	array = (int*) malloc(size * sizeof(int));
+
+	if(array == NULL){
+		printf("Malloc failed");
+		return NULL;
+	} else {
+		for(int i=0; i < size; i++){
+			array[i] = (rand() % 10);
+		}
+		return array;
+	}
+}
+
+
+/*
+	Inverts the values of a given segment
+	Receibes a void pointer that is cast to a Segment pointer to supress some compiler warnings.
+*/
+void *invert(void *seg)
+{
+	Segment *segment = (Segment *)seg;
+	int temp;
+	int start = 0;
+	int end = segment->size - 1;
+
+	while (start < end)
+	{
+		temp = segment->values[start];
+		segment->values[start] = segment->values[end];
+		segment->values[end] = temp;
+		start++;
+		end--;
+	}
+
+	return NULL;
+}
+
+/*
+	Splits the given array in n equal segments.
+	The last segment also carries the remainder of the division if necessary.
+*/
+Segment *split(int *array, int n, int size)
+{
+	Segment *segments;
+	int segment_size = (int)(size / n);
+	int remainder = (int)(size % n);
+
+	segments = malloc(n * sizeof(Segment));
+	int arr_index = 0;
+
+	Segment *seg;
+	for (int i = 0; i < n; ++i)
+	{
+		seg = malloc(sizeof(Segment));
+		seg->thread_id = i;
+
+		if (i < n - 1)
+			seg->size = segment_size;
+		else
+			seg->size = segment_size + remainder;
+
+		seg->values = malloc(segment_size * sizeof(int));
+
+		for (int j = 0; j < seg->size; ++j)
+		{
+			seg->values[j] = array[arr_index];
+			arr_index++;
+		}
+		segments[i] = *seg;
+		free(seg);
+	}
+	return segments;
+}
+
+/*
+	Reassambles a list of segments into a single unidimentional array (makes printing easier).
+*/
+int *reassamble(Segment *segments, int n_segments, int final_size)
+{
+	int *result = malloc(final_size * sizeof(int));
+	int i = 0;
+	for (int j = n_segments - 1; j >= 0; --j)
+	{
+		for (int k = 0; k < segments[j].size; ++k)
+		{
+			result[i] = segments[j].values[k];
+			i++;
+		}
+	}
+	return result;
+}
+
+/*Driver code*/
+int main(int argc, char **argv)
+{
+	srand(time(NULL));
+	int array_size = atoi(argv[1]);
+	int n = atoi(argv[2]);
+
+	//Optimizes the number of threads
+	if (n > array_size)
+	{
+		n = array_size;
+	}
+
+	int *arr = random_array(array_size);
+	print_array(arr, array_size);
+
+	Segment *segments = split(arr, n, array_size);
+	pthread_t threads[n];
+
+	for (int i = 0; i < n; ++i)
+	{
+		pthread_create(&threads[n], NULL, &invert, (void *)&segments[i]);
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		pthread_join(threads[n], NULL);
+	}
+
+	int *inverted;
+	inverted = reassamble(segments, n, array_size);
+	print_array(inverted, array_size);
+
+	free(segments);
+	return EXIT_SUCCESS;
+}
