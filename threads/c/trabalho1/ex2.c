bd8c622402e03e1c14673e8cf9a51fd058be0549
Rafael Teixeira <rodriguesteixeirarafael@gmail.com>
2020-11-21 12:20:13 -0300

    ex2.c done

--- threads/c/trabalho1/ex2.c
+++ threads/c/trabalho1/ex2.c
@@ -1,6 +1,7 @@
 #include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
-#include "lib/helpers.h"
 
 typedef struct Pair
 {
@@ -8,23 +9,41 @@ typedef struct Pair
     int y;
 } Pair;
 
-typedef struct Segment
+typedef struct arg_struct
 {
-    int thread_id;
-    int size;
-    Pair *pairs;
-} Segment;
+    int thread_id, start, end, n, p;
+    Pair *map;
+    int **A;
+    int **B;
+    int **C;
+} arg_struct;
 
+void print_matrix(int **A, int m, int n)
+{
+    for (int i = 0; i < m; ++i)
+    {
+        printf("\t");
+        for (int j = 0; j < n; ++j)
+        {
+            printf("%d ", A[i][j]);
+        }
+        printf("\n");
+    }
+}
 int **random_matrix(int m, int n)
 {
+    int *r;
     int **matrix;
     matrix = malloc(m * n * sizeof(int));
 
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < m; ++i)
     {
-        int *rand;
-        rand = random_array(n);
-        matrix[i] = rand;
+        r = malloc(n * sizeof(int));
+        for (int j = 0; j < n; ++j)
+        {
+            r[j] = (int)(rand() % 10);
+        }
+        matrix[i] = r;
     }
 
     return matrix;
@@ -33,23 +52,28 @@ int **random_matrix(int m, int n)
 int multiply_and_sum(int *a, int *b, int size)
 {
     int sum = 0;
+
     for (int i = 0; i < size; i++)
     {
         sum += (a[i] * b[i]);
     }
+    return sum;
 }
 
 /*
-    Creates a representation of the resulting matrix in the form of a pair array kinda like this:
+    Creates a representation of the resulting matrix in the form of a index pair array kinda like this:
     {(0,0), (0,1) ... (0, n), (1, 0), (1, 1) ... (n, n)}
 */
-Pair* squash_matrix(int l, int c){
-    Pair* pairs;
-    pairs = malloc((l*c) * sizeof(Pair));
-    Pair* pair;
+Pair *squash_matrix(int l, int c)
+{
+    Pair *pairs;
+    pairs = malloc((l * c) * sizeof(Pair));
+    Pair *pair;
     int pair_count = 0;
-    for(int i = 0; i < l; ++i){
-        for(int j = 0; j < c; ++j){
+    for (int i = 0; i < l; ++i)
+    {
+        for (int j = 0; j < c; ++j)
+        {
             pair = malloc(sizeof(Pair));
             pair->x = i;
             pair->y = j;
@@ -62,29 +86,36 @@ Pair* squash_matrix(int l, int c){
     return pairs;
 }
 
-Segment* split(int l, int c, int n){
-    int segment_size = (int)((l*c) / n);
-    int remainder = (int)((l*c) % n);
-    Segment segments[n];
-    
-    int seg_fill = 0;
-    Segment* seg;
-    Pair* pairs = squash_matrix(l, c);
-    int pair_index = 0;
-    for(int s = 0; s < n; ++s){
-        seg = malloc(sizeof(Segment));
-        if(s < n - 1){
-            seg->size = segment_size;
-        }else{
-            seg->size = segment_size + remainder;
+void *process(void *arguments)
+{
+    arg_struct *args = (arg_struct *)arguments;
+    int *line;
+    int *column;
+    int n = args->n;
+    int p = args->p;
+    int start = args->start;
+    int end = args->end;
+    Pair *map = args->map;
+    int **A = args->A;
+    int **B = args->B;
+    int **C = args->C;
+
+    for (int i = start; i <= end; ++i)
+    {
+        line = malloc(n * sizeof(int));
+        column = malloc(p * sizeof(int));
+        for (int j = 0; j < n; ++j)
+        {
+            line[j] = A[map[i].x][j];
         }
-        seg->pairs = malloc(segment_size * sizeof(Pair));
-        for(int i = 0; i < seg->size; ++i){
-            seg->pairs[i] = pairs[pair_index];
-            ++pair_index;
+        for (int k = 0; k < p; ++k)
+        {
+            column[k] = B[k][map[i].y];
         }
-    }
-    return segments;
+        C[map[i].x][map[i].y] = multiply_and_sum(line, column, n);
+        free(line);
+        free(column);
+    };
 }
 
 int main(int argc, char **argv)
@@ -98,41 +129,61 @@ int main(int argc, char **argv)
 
     int **A;
     int **B;
+    int **C = random_matrix(m, q);
+
     A = random_matrix(m, n);
 
     printf("A:\n");
-    for (int i = 0; i < m; ++i)
-    {
-        printf("\t");
-        for (int j = 0; j < n; ++j)
-        {
-            printf("%d ", A[i][j]);
-        }
-        printf("\n");
-    }
-
+    print_matrix(A, m, n);
     printf("\nB:\n");
     B = random_matrix(p, q);
-    for (int i = 0; i < m; ++i)
-    {
-        printf("\t");
-        for (int j = 0; j < n; ++j)
-        {
-            printf("%d ", A[i][j]);
-        }
-        printf("\n");
-    }
+    print_matrix(B, p, q);
+
     if (n_threads > m * q)
     {
         n_threads = m * q;
     }
 
-    int C[m][q];
-    for(int i = 0; i < m; ++i){
-        for(int j = 0; j < q; ++j){
-            
+    Pair *index_map = squash_matrix(m, q);
+    pthread_t threads[n_threads];
+    arg_struct *args;
+
+    int segment_size = (int)(m * q) / n_threads;
+    int remainder = (int)(m * q) % n_threads;
+    int start = 0;
+    int end = -1;
+
+    for (int i = 0; i < n_threads; ++i)
+    {
+        if (i == n_threads - 1) //last segment carries the remainder
+        { 
+            segment_size += remainder;
         }
+        end += segment_size;
+
+        args = malloc(sizeof(arg_struct));
+        args->thread_id = i;
+        args->n = n;
+        args->p = p;
+        args->start = start;
+        args->end = end;
+        args->map = index_map;
+        args->A = A;
+        args->B = B;
+        args->C = C;
+
+        pthread_create(&threads[i], NULL, &process, (void *)args);
+
+        start = end + 1;
     }
 
+    for (int i = 0; i < n_threads; ++i)
+    {
+        pthread_join(threads[i], NULL);
+    }
+
+    free(args);
+    printf("C:\n");
+    print_matrix(C, m, q);
     return EXIT_SUCCESS;
 }
